2e1a4198ef2130cf807c4bb0786ce1354ad51d22
rk <rebeccakruk@outlook.com>
2022-11-08 17:51:20 -0700

    simple user array assignment complete and working

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 let userNames = [`Boo`, `Snoop`, `Nines`, `Beans`, `Kalley`, `Kate`, `Anna`, `Ally`, `Molly`, `Andie`];
 let userAges = [8, 2, 2, 1, 11, 22, 18, 18, 16, 9];
 
-// counter = 0;
-// while (counter < userAges.length){
-//   if (userAges[counter] >= 18){
-//     console.log(userNames[counter]);
-//     }
-//     counter++;
-//  }
 
+// the while loop contains ages of AT LEAST 18, including 18
+counter = 0;
+while (counter < userAges.length){
+  if (userAges[counter] >= 18){
+    console.log(userNames[counter]);
+    }
+    counter++;
+ }
+
+
+// the for loop only has those who are under 18
 for(let counter = 0; counter < userAges.length; counter++){
-    if (userAges <= 18) {
+    if (userAges[counter] < 18) {
         console.log(userNames[counter]);
 }
 }
